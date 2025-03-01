7c96fb9d6a166c15b168e19b7762d3fd7ee5df8a
rohitBanikMazumder-2003 <rohitbanikmazumder@gmail.com>
2025-03-01 10:49:01 +0530

    Updated app.js: Fixed home route and listings path issue

--- app.js
+++ app.js
@@ -1,74 +1,74 @@
 require('dotenv').config();
 
-const express=require('express');
-const app=express()
-const mongoose=require('mongoose')
-// const PORT = process.env.PORT || 3000
-const path=require("path")
+const express = require('express');
+const app = express()
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3000
+const path = require("path")
 
-const methodOverride=require("method-override")
-const engine=require("ejs-mate")
+const methodOverride = require("method-override")
+const engine = require("ejs-mate")
 
-const expressError=require("./utils/expressError")
+const expressError = require("./utils/expressError")
 
+const listingsRouter = require("./routes/listing")
+const reviewsRouter = require("./routes/review")
+const userRouter = require("./routes/user")
 
+const session = require("express-session")
+const MongoStore = require("connect-mongo")
+const flash = require("connect-flash")
 
-const listingsRouter=require("./routes/listing")
-const reviewsRouter=require("./routes/review")
-const userRouter=require("./routes/user")
+const passport = require("passport")
+const LocalStrategy = require("passport-local")
+const User = require("./models/user.js")
 
-const session=require("express-session")
-const MongoStore=require("connect-mongo")
-const flash=require("connect-flash")
+const dbUrl = process.env.ATLASDB_URL
 
-const passport=require("passport")
-const LocalStrategy=require("passport-local")
-const User=require("./models/user.js")
-
-const dbUrl=process.env.ATLASDB_URL
+const Listing = require("./models/listing");
 
 main()
-.then(()=>{
+  .then(() => {
     console.log("connection succesful")
-})
-.catch(err => console.log(err));
+  })
+  .catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect(dbUrl);
 }
 
 //middlewares
-app.set("view engine","ejs")
-app.set("views",path.join(__dirname,"views"))
-app.use(express.urlencoded({extended:true}));
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"))
 app.engine('ejs', engine);
-app.use(express.static(path.join(__dirname,"/public")))
+app.use(express.static(path.join(__dirname, "/public")))
 
-const store=MongoStore.create({
-  mongoUrl:dbUrl,
-  crypto:{
-    secret:process.env.SECRET
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET
   },
-  touchAfter:24*3600
+  touchAfter: 24 * 3600
 
 })
 
 
-store.on("error",()=>{
-  console.log("ERROR IN SESSION MONGO STORE",err);
+store.on("error", () => {
+  console.log("ERROR IN SESSION MONGO STORE", err);
 })
 
-const sessionOptions={
+const sessionOptions = {
   store,
-  secret:process.env.SECRET,
-  resave:false,
-  saveUninitialized:true,
-  cookie:{
-    expires:Date.now()+7*24*60*60*1000,
-    maxAge:7*24*60*60*1000,
-    httpOnly:true
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true
   }
 }
 
@@ -86,32 +86,32 @@ passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
 
-app.use((req,res,next)=>{
-  res.locals.success=req.flash("success")
-  res.locals.error=req.flash("error")
-  res.locals.currUser=req.user
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success")
+  res.locals.error = req.flash("error")
+  res.locals.currUser = req.user
   next()
 })
 
 
-app.use("/listing",listingsRouter)
-app.use("/listings/:id/reviews",reviewsRouter)
-app.use("/",userRouter)
+app.use("/listing", listingsRouter)
+app.use("/listings/:id/reviews", reviewsRouter)
+app.use("/", userRouter)
 
 app.get("/", (req, res) => {
-  res.render("listings/index"); // Ensure you have a 'home.ejs' file in the 'views' folder
+  res.redirect("/listing");
 });
 
 
-app.all("*",(req,res,next)=>{
-  next(new expressError(404,"Page not found!"))
+app.all("*", (req, res, next) => {
+  next(new expressError(404, "Page not found!"))
 })
 
-app.use((err,req,res,next)=>{
-  let {statusCode=500,message="something went wrong!"}=err;
-  res.status(statusCode).render("error.ejs",{message})
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "something went wrong!" } = err;
+  res.status(statusCode).render("error.ejs", { message })
 })
 
-app.listen(3000,()=>{
-  console.log(`app listening on port 3000`)
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}`)
 })
--- routes/listing.js
+++ routes/listing.js
@@ -1,46 +1,46 @@
-const express=require("express")
-const router=express.Router()
+const express = require("express")
+const router = express.Router()
 
-const wrapAsync=require("../utils/wrapAsync")
+const wrapAsync = require("../utils/wrapAsync")
 
-const Listing=require("../models/listing")
-const {isLoggedIn, isOwner,validateListing}=require("../middleware.js")
+const Listing = require("../models/listing")
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js")
 
-const listingController=require("../controllers/listing.js")
+const listingController = require("../controllers/listing.js")
 
 
-  
+
 router.route("/")
   //get all listings 
   .get(wrapAsync(listingController.index))
   //create new route
-  .post(isLoggedIn,validateListing,wrapAsync(listingController.createNewListing))
+  .post(isLoggedIn, validateListing, wrapAsync(listingController.createNewListing))
 
-  
-  
+
+
 //new route 
-router.get("/new",isLoggedIn,wrapAsync(listingController.renderNewForm))
+router.get("/new", isLoggedIn, wrapAsync(listingController.renderNewForm))
 
 
 router.route("/:id")
   //show route
   .get(wrapAsync(listingController.showListing))
   //update route
-  .put(isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing))
+  .put(isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing))
   //destroy route
-  .delete(isLoggedIn,isOwner,wrapAsync(listingController.deleteListing))
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.deleteListing))
 
 
 
-  
-  
-  //edit route
-  router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync(listingController.renderEditForm))
-
-  
-  
-
-  
 
 
-  module.exports=router
+//edit route
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm))
+
+
+
+
+
+
+
+module.exports = router
